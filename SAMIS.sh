41b44bfb52c5944bb72de1ffd180c8a47437392c
Shivam <70562711+SamIsTheRetard@users.noreply.github.com>
2022-04-09 00:04:53 +0530

    added script to get Material Design web font

--- SAMIS.sh
+++ SAMIS.sh
@@ -45,8 +45,8 @@ pacstrap /mnt nano git base linux linux-firmware networkmanager dhcpcd ifplugd w
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-sed -n '58,100p;101q' SAMIS.sh  > /mnt/SAMIS_PART2.sh
-sed -n '101,144p;145q' SAMIS.sh  > /mnt/SAMIS_PART3.sh
+sed -n '58,115p;116q' SAMIS.sh  > /mnt/SAMIS_PART2.sh
+sed -n '116,156p;157q' SAMIS.sh  > /mnt/SAMIS_PART3.sh
 
 chmod +x /mnt/SAMIS_PART2.sh
 chmod +x /mnt/SAMIS_PART3.sh
@@ -58,6 +58,10 @@ exit
 #PART2
 
 clear
+
+sudo timedatectl set-ntp true
+sudo timedatectl set-timezone Asia/Calcutta
+sudo hwclock --systohc
 
 systemctl enable NetworkManager
 
@@ -77,18 +81,29 @@ grub-mkconfig -o /boot/grub/grub.cfg
 sleep 5
 pacman -Sy --noconfirm sed
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/g" /etc/pacman.conf
-pacman -Sy --noconfirm xorg xorg-server xorg-xinit nitrogen picom chromium neofetch python-pywal htop wget jq xdotool dunst base-devel pamixer maim xclip libnotify pulseaudio pulseaudio-alsa alsa-utils libpulse pavucontrol gvfs ntfs-3g openssh brightnessctl noto-fonts-cjk noto-fonts-emoji noto-fonts sxiv mtpfs ttf-nerd-fonts-symbols curl mpv rclone redshift xf86-input-synaptics pcmanfm zip unzip unrar p7zip ffmpeg imagemagick dosfstools slock arc-gtk-theme papirus-icon-theme aria2 mpd ncmpcpp xdg-user-dirs rsync gvfs-mtp ranger ueberzug
+pacman -Sy --noconfirm xorg xorg-server xorg-xinit nitrogen picom chromium neofetch python-pywal htop wget jq xdotool dunst base-devel pamixer maim xclip libnotify pulseaudio pulseaudio-alsa alsa-utils libpulse pavucontrol gvfs ntfs-3g openssh brightnessctl noto-fonts-cjk noto-fonts-emoji noto-fonts sxiv mtpfs ttf-nerd-fonts-symbols curl mpv rclone redshift xf86-input-synaptics pcmanfm zip unzip unrar p7zip ffmpeg imagemagick dosfstools slock arc-gtk-theme papirus-icon-theme aria2 mpd ncmpcpp xdg-user-dirs rsync gvfs-mtp ranger ueberzug zsh
+
+TMPFILE=`mktemp`
+PWD=`pwd`
+wget "https://codeload.github.com/Templarian/MaterialDesign-Webfont/zip/refs/heads/master" -O $TMPFILE
+unzip -d $PWD $TMPFILE
+rm $TMPFILE
+sudo mkdir -p /usr/local/share/fonts
+sudo mv MaterialDesign-Webfont-master/fonts/* /usr/local/share/fonts/
+rm -rf MaterialDesign-Webfont-master/
+fc-cache -f
 
 systemctl enable NetworkManager
 
 echo "Enter username: "
 read username
 useradd -m $username
+echo "Enter password for your username: "
 passwd $username
 sed -i "s/^GROUP=.*/GROUP=users/g" /etc/default/useradd
 usermod -aG users $username
 echo "$username ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$username
-echo "Make the root password:"
+echo "Make the root password: "
 passwd
 
 mv SAMIS_PART3.sh /home/$username/
@@ -122,24 +137,20 @@ cd ..
 paru -S libxft-bgra-git jmtpfs nerd-fonts-jetbrains-mono
 
 ln -s ~/.config/x11/xinitrc .xinitrc
-echo "What do you want to use for setting up zsh? zsh4human[1] Oh-My-ZSH[2]
+echo "What do you want to use for setting up zsh? zsh4human[1] Oh-My-ZSH[2]"
 read ans
 if [ "$ans" == '1' ]; then
-	if command -v curl >/dev/null 2>&1; then
-	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-	else
-	  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-	fi
+	sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 	echo "alias dotfiles='/usr/sbin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> .zshrc
 	echo "path+=(/home/samisthefbi/.local/bin)" >> .zshrc
 	echo "export PATH" >> .zshrc
+	alias tmpdots='/usr/sbin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+	tmpdots config --local status.showUntrackedFiles no
 elif
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-alias tmpdots='/usr/sbin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-tmpdots config --local status.showUntrackedFiles no
 
-echo "You are somewhat done. Restart X & hope for the best!"
+echo "You are somewhat done. startx & hope for the best!"
 
 exit
 
